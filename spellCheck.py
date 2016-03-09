ef0c70550e25189542a0b38973aa803b319c25c2
murray16s <murray16s@ncssm.edu>
2016-03-09 12:00:34 -0500

    basic spellChecker

--- spellCheck.py
+++ spellCheck.py
@@ -1,1 +1,16 @@
 #!/usr/bin/env python
+import sys
+words = []
+with open("words", "r") as wordFile:
+    for line in wordFile:
+        line  = line[0:-1]
+        words.append(line.lower())
+with open("testFile", "r") as testFile:
+    lineNum = 0
+    for line in testFile:
+        lineNum += 1
+        line = line.split()
+        for word in line:
+            word = word.lower()
+            if not(word in words):
+                print(str(lineNum) + ": " + word)
