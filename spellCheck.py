464ecda6bcb23ddd430ddbedc5f8ed691177c3c0
murray16s <murray16s@ncssm.edu>
2016-03-10 09:28:28 -0500

    command line arguments are now accepted.

--- spellCheck.py
+++ spellCheck.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python
 import sys
 words = []
-with open("words", "r") as wordFile:
+wordFileName = sys.argv[2]
+testFileName = sys.argv[1]
+with open(wordFileName, "r") as wordFile:
     for line in wordFile:
         line  = line[0:-1]
         words.append(line.lower())
-with open("testFile", "r") as testFile:
+with open(testFileName, "r") as testFile:
     lineNum = 0
     for line in testFile:
         lineNum += 1
