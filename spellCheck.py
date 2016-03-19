d2e3a232a7941542d758d923600ad7f9151c4f6b
murray16s <murray16s@ncssm.edu>
2016-03-19 13:21:57 -0400

    implementing backwards check

--- spellCheck.py
+++ spellCheck.py
@@ -5,32 +5,55 @@ words = []
 sym = ["!","@","#", "$", "%", "^", "&", "*", "(", ")", "{", "}", "[", "]", "\/",".", ",", "?", "<", ">", ":", ";"]
 wordFileName = sys.argv[2]
 testFileName = sys.argv[1]
+outFileName =  sys.argv[3]
 with open(wordFileName, "r") as wordFile:
     for line in wordFile:
         line  = line[0:-1]
         words.append(line.lower())
-trie = marisa_trie.Trie(words)
-suggestions = []
-sug = []
+
+
 with open(testFileName, "r") as testFile:
-    lineNum = 0
-    for line in testFile:
-        lineNum += 1
-        line = line.split()
-        for word in line:
-            for char in word:
-                if(char in sym):
-                    word = word.replace(char, "")
-            word = word.lower()
-            if not(word in trie):
-                print(str(lineNum) + ": " + word)
-                for i  in range(1, len(word)-1):
-                    #print("**********" + i + "**********")
-                    temp = trie.keys(word[0:len(word)-i])
-                    #print(temp)
-                    for key in temp:
-                        sug.append(key)
-                    for key in sug:
-                        if not(key in suggestions) and (len(suggestions) <= 10):
+    with open(outFileName, "w") as outFile:
+        trie = marisa_trie.Trie(words)
+        lineNum = 0
+        for line in testFile:
+            lineNum += 1
+            line = line.split()
+            for word in line:
+                for char in word:
+                    if(char in sym):
+                        word = word.replace(char, "")
+                word = word.lower()
+                if not(word in trie):
+                    suggestions = []
+                    sug = []
+                    outFile.write(str(lineNum) + ": " + word + "\n")
+                    for i  in range(1, len(word)-1):
+                        #print("**********" + i + "**********")
+                        temp = trie.keys(word[0:len(word)-i])
+                        #print(temp)
+                        for key in temp:
+                            sug.append(key)
+                        for key in sug:
+                            if not(key in suggestions) and (len(suggestions) <= 10):
+                                suggestions.append(key)
+                    backWords = []
+                    for word in words:
+                        backWords.append(word[::-1])
+                    trie = marisa_trie.Trie(backWords)
+                    backSug = []
+                    backSuggestions = []
+                    for i  in range(1, len(word)-1):
+                        #print("**********" + i + "**********")
+                        temp = trie.keys(word[0:len(word)-i])
+                        #print(temp)
+                        for key in temp:
+                            backSug.append(key)
+                        for key in backSug:
+                            if not(key in backSuggestions) and (len(backSuggestions) <= 10):
+                                backSuggestions.append(key)
+                    for key in backSuggestions:
+                        if key not in suggestions:
                             suggestions.append(key)
-                print(suggestions)
+                    outFile.write(str(suggestions) + "\n")
+                    suggestions = []
